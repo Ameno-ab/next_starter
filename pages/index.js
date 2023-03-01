b0bbcf2173ef1322f62ace75eaa15261aa91c1dc
Ameno-ab <99277062+Ameno-ab@users.noreply.github.com>
2023-03-01 21:52:13 +0300

    ewr

--- pages/index.js
+++ pages/index.js
@@ -26,10 +26,6 @@ export default function Home() {
           </p>
           <div>
             <a
-                                         
-              
-              
-              
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
