a0d09470921e38276e3916b25b2cb5ee08fd7fa2
Ameno-ab <99277062+Ameno-ab@users.noreply.github.com>
2023-03-10 20:22:05 +0300

    app scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
 import "@/styles/globals.css"
+// fdf
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
