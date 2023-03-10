875e562dd2d86e371c2d902ad32f944627c5f062
Ameno-ab <99277062+Ameno-ab@users.noreply.github.com>
2023-03-10 20:42:51 +0300

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
 import "@/styles/globals.css"
-// fdf fgfg fdf gedd dsd
+// fdf fgfg fdf gedd dsd hhh
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
